5e5cd5649de05c2d90c7ea38dbebd5c077cdc598
Matthew <caspr117@gmail.com>
2025-11-02 10:06:23 -0500

    Clean Up route traffic

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+
+
+export function signUp(req,res){
+    res.send("Signup Route")
+}
+export function login(req,res){
+    res.send("Login Route")
+}
+export function logOut(req,res){
+    res.send("Logout route")
+}
