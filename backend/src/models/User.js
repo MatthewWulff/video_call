cee6f0894da39c3a991b63fe3deb5b9952c891fa
Matthew <caspr117@gmail.com>
2025-11-02 10:50:55 -0500

    Basic User Schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      typer: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    nativeLanguage: {
+      type: String,
+      default: "",
+    },
+    learningLanguage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    isOnBoarded: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
